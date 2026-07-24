e9c2fb15776b28fdeb08054060296c609aeeec22
Ali <lyasghrtwslpnahy@gmail.com>
2026-07-24 06:08:11 -0700

    update board to match cell

--- board.py
+++ board.py
@@ -1,21 +1,4 @@
-class Cell:
-    def __init__(self, row, col):
-        self.row = row
-        self.col = col
-        self.has_plant = False  # آیا روی این خانه گیاهی کاشته شده؟
-
-    def get_row(self):
-        return self.row
-
-    def get_col(self):
-        return self.col
-
-    def get_has_plant(self):
-        return self.has_plant
-
-    def set_has_plant(self, status):
-        self.has_plant = status
-
+from cell import Cell
 
 class Board:
     def __init__(self, rows, cols):
@@ -24,5 +7,12 @@ class Board:
         # ساخت جدول دو بعدی از سلول‌ها
         self.grid = [[Cell(i, j) for j in range(cols)] for i in range(rows)]
 
-    def display_board_status(self):
-        print(f"Board created with {self.rows} rows and {self.cols} cols.")
+    def display_board(self):
+        print("=== GAME BOARD ===")
+        for row in self.grid:
+            row_str = ""
+            for cell in row:
+                # استفاده از متد is_empty یا بررسی مستقیم self.plant برای نمایش وضعیت
+                status = "." if cell.is_empty() else "P"
+                row_str += f"[{status}] "
+            print(row_str)
